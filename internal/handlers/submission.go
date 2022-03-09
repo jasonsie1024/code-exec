070a5bfec18408d2b19659ecb0634f5c189acffb
Jason Hsieh <jason.plainlog@gmail.com>
2022-03-10 01:08:02 +0800

    get /submission/{token} route complete

--- internal/handlers/submission.go
+++ internal/handlers/submission.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
 
 	"cloud.google.com/go/storage"
+	"github.com/jason-plainlog/code-exec/internal/config"
 	"github.com/jason-plainlog/code-exec/internal/models"
 	"github.com/labstack/echo/v4"
 )
@@ -15,7 +17,7 @@ type SubmissionHandler struct {
 func (h *SubmissionHandler) RegisterRoutes(e *echo.Echo) {
 	e.POST("/submission", h.Create)
 	e.GET("/submission/:token", h.Get)
-	e.GET("/task/:token", h.Get)
+	e.GET("/task/:token", h.GetTask)
 }
 
 func (h *SubmissionHandler) Create(c echo.Context) error {
@@ -52,12 +54,59 @@ func (h *SubmissionHandler) Create(c echo.Context) error {
 
 func (h *SubmissionHandler) Get(c echo.Context) error {
 	token := c.Param("token")
+	config := config.GetConfig()
 
-	return c.JSON(http.StatusOK, token)
+	// get submission first
+	submissionBucket := h.Storage.Bucket(config.SubmissionBucket)
+	submission := models.StoredSubmission{}
+	err := models.StorageRead(submissionBucket.Object(token), &submission)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "submission not found")
+	}
+
+	// get task results
+	taskBucket := h.Storage.Bucket(config.TaskBucket)
+	resultsChan := make([]chan models.Result, len(submission.Tasks))
+	for i, token := range submission.Tasks {
+		resultsChan[i] = make(chan models.Result, 1)
+		go func(token string, resultChan chan models.Result) {
+			result := models.Result{}
+			models.StorageRead(taskBucket.Object(token), &result)
+			resultChan <- result
+		}(token.String(), resultsChan[i])
+	}
+
+	// form final response
+	tasks := []map[string]interface{}{}
+	for i := range submission.Tasks {
+		result := <-resultsChan[i]
+		tasks = append(tasks, map[string]interface{}{
+			"token":  result.Token,
+			"time":   result.Time,
+			"memory": result.Memory,
+			"status": result.Status,
+		})
+	}
+	response := map[string]interface{}{
+		"token":       submission.Token,
+		"timestamp":   submission.Timestamp,
+		"source_code": submission.SourceCode,
+		"language":    submission.Langauge,
+		"tasks":       tasks,
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
 
 func (h *SubmissionHandler) GetTask(c echo.Context) error {
 	token := c.Param("token")
+	config := config.GetConfig()
+	taskBucket := h.Storage.Bucket(config.TaskBucket)
 
-	return c.JSON(http.StatusOK, token)
+	reader, err := taskBucket.Object(token).NewReader(context.Background())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "task not found")
+	}
+
+	return c.Stream(http.StatusOK, "application/json", reader)
 }
